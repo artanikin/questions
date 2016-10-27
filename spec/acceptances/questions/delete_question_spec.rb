e29d21b19d68ed8785f87dc8398f7e72b549936f
Anikin Artyom <anikinartyom@gmail.com>
2016-10-27 08:18:31 +0500

    remove noize from delete question acceptance test

--- spec/acceptances/questions/delete_question_spec.rb
+++ spec/acceptances/questions/delete_question_spec.rb
@@ -6,12 +6,10 @@ feature 'Delete question', %(
   I can to able to remove question
 ) do
 
-  given(:user) { create(:user_with_questions, question_count: 1) }
+  given!(:user) { create(:user_with_questions, question_count: 1) }
 
   scenario 'Not-authorized user does not remove question' do
-    user
     visit questions_path
-
     expect(page).not_to have_content 'Remove question'
   end
 
@@ -28,9 +26,10 @@ feature 'Delete question', %(
   end
 
   scenario 'Not-author can not remove not his question' do
-    user
     new_user = create(:user)
     sign_in(new_user)
+
+    visit questions_path
 
     expect(page).to have_content('Simple title', count: 1)
     expect(page).to_not have_link 'Remove question'
