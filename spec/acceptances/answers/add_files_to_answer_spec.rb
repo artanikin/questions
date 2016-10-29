cb73bc770acb51288bf58aee139954f2f34d1816
Anikin Artyom <anikinartyom@gmail.com>
2016-10-29 23:51:02 +0500

    Test for add many files to answer

--- spec/acceptances/answers/add_files_to_answer_spec.rb
+++ spec/acceptances/answers/add_files_to_answer_spec.rb
@@ -17,10 +17,26 @@ feature 'Add files to answers', %(
   scenario 'User adds file when asks the question', :js do
     fill_in 'Body', with: 'Placeholder for answer body'
     attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+
+    click_on 'add file'
+
+    within '.nested_fields:nth-child(2)' do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on 'add file'
+
+    within '.nested_fields:nth-child(3)' do
+      attach_file 'File', "#{Rails.root}/spec/acceptance_helper.rb"
+      click_on 'remove file'
+    end
+
     click_on 'Create Answer'
 
     within '#answers' do
       expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
+      expect(page).to_not have_link 'acceptance_helper.rb', href: '/uploads/attachment/file/3/acceptance_helper.rb'
     end
   end
 end
