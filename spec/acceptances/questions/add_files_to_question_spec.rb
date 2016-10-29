7e423519d7509348e7ae6e4f8b320dae6ce24866
Anikin Artyom <anikinartyom@gmail.com>
2016-10-29 22:43:41 +0500

    Acceptance test for add many files for question

--- spec/acceptances/questions/add_files_to_question_spec.rb
+++ spec/acceptances/questions/add_files_to_question_spec.rb
@@ -13,12 +13,28 @@ feature 'Add files to questions', %(
     visit new_question_path
   end
 
-  scenario 'User adds file when asks the question' do
+  scenario 'User adds files when asks the question', :js do
     fill_in 'Title', with: 'Title placeholder'
     fill_in 'Body', with: 'Placeholder for body'
     attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+
+    click_on 'add file'
+
+    within '.nested_fields:nth-child(2)' do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on 'add file'
+
+    within '.nested_fields:nth-child(3)' do
+      attach_file 'File', "#{Rails.root}/spec/acceptance_helper.rb"
+      click_on 'remove file'
+    end
+
     click_on 'Create'
 
     expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
+    expect(page).to_not have_link 'acceptance_helper.rb', href: '/uploads/attachment/file/3/acceptance_helper.rb'
   end
 end
