6df7630331199779db6b5c86dae2c18925400606
Anikin Artyom <anikinartyom@gmail.com>
2016-11-10 19:05:38 +0500

    add voted to answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,6 +2,8 @@ class AnswersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_answer, except: [:create]
 
+  include Voted
+
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.new(answer_params.merge(author: current_user))
