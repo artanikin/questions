c8d9d0c304b9c49a0939f5e0b3b4210f533e26c5
Anikin Artyom <anikinartyom@gmail.com>
2016-11-10 15:31:27 +0500

    fix scope

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -4,7 +4,7 @@ module Votable
   included do
     has_many :votes, as: :votable, dependent: :destroy
 
-    scope :with_rating, lambda do
+    scope :with_rating, -> do
       left_joins(:votes)
         .select("#{table_name}.*, COALESCE(SUM(votes.value), 0) AS rating")
         .group("#{table_name}.id, votes.votable_type, votes.votable_id")
