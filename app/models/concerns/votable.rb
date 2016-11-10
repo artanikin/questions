7f8a847ebe78f2b6bea8aed2e1594a34f6ac4665
Anikin Artyom <anikinartyom@gmail.com>
2016-11-10 15:25:42 +0500

    add votable concern

---
+++ app/models/concerns/votable.rb
@@ -0,0 +1,18 @@
+module Votable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :votes, as: :votable, dependent: :destroy
+
+    scope :with_rating, lambda do
+      left_joins(:votes)
+        .select("#{table_name}.*, COALESCE(SUM(votes.value), 0) AS rating")
+        .group("#{table_name}.id, votes.votable_type, votes.votable_id")
+        .order('created_at ASC')
+    end
+  end
+
+  def evaluation
+    votes.sum(:value)
+  end
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,29 +1,10 @@
 class Question < ApplicationRecord
   include Attachable
   include Authorable
+  include Votable
 
   has_many :answers, -> { order('best desc') }, dependent: :destroy
-  has_many :votes, as: :votable, dependent: :destroy
 
   validates :title, :body, presence: true
   validates :title, :body, length: { minimum: 10 }
-
-  scope :with_rating, -> do
-    left_joins(:votes)
-      .select('questions.*, COALESCE(SUM(votes.value), 0) AS rating')
-      .group('questions.id, votes.votable_type, votes.votable_id')
-      .order('created_at ASC')
-  end
-
-  def evaluation
-    votes.sum(:value)
-  end
-
-  # def vote_up(user)
-  #   if !user.author?(self) && votes.create(author: user, value: 1)
-  #     true
-  #   else
-  #     false
-  #   end
-  # end
 end
