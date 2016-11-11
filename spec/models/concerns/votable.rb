9f2a608841d2aa1fc9caf0458e8f72add727a60e
Anikin Artyom <anikinartyom@gmail.com>
2016-11-11 18:16:49 +0500

    refactoring test votable

--- spec/models/concerns/votable.rb
+++ spec/models/concerns/votable.rb
@@ -1,34 +1,22 @@
 shared_examples_for 'votable' do
-  let(:first_user) { create(:user) }
-  let(:second_user) { create(:user) }
+  let(:user) { create(:user) }
   let(:model) { create(described_class.to_s.underscore.to_sym) }
 
   it { should have_many(:votes) }
 
   describe '#with_rating' do
     it 'has field rating' do
-      create(:vote, votable: model, value: 1, author: first_user)
+      create(:vote, votable: model, value: 1, author: user)
       expect(described_class.with_rating.first.has_attribute?(:rating)).to be(true)
     end
   end
 
-  describe '#evaluation' do
-    it 'positive' do
-      create(:vote, votable: model, value: 1, author: first_user)
-      create(:vote, votable: model, value: 1, author: second_user)
+  it '#evaluation' do
+    create(:vote, votable: model, value: 1, author: user)
+    create(:vote, votable: model, value: 1, author: create(:user))
+    create(:vote, votable: model, value: 1, author: create(:user))
+    create(:vote, votable: model, value: -1, author: create(:user))
 
-      expect(model.evaluation).to eq(2)
-    end
-
-    it 'negative' do
-      create(:vote, votable: model, value: -1, author: first_user)
-      create(:vote, votable: model, value: -1, author: second_user)
-
-      expect(model.evaluation).to eq(-2)
-    end
-
-    it 'zero' do
-      expect(model.evaluation).to be_zero
-    end
+    expect(model.evaluation).to eq(2)
   end
 end
