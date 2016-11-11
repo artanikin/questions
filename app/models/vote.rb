6822ea97191aef4c94fb822bce4f69d79a506845
Anikin Artyom <anikinartyom@gmail.com>
2016-11-11 18:05:16 +0500

    remove presence validation for value

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,9 +4,7 @@ class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
 
   validates :votable_type, inclusion: { in: %w(Question Answer) }
-  validates :value, presence: true
   validates :value, inclusion: { in: [-1, 1] }
-  validates :value, presence: true, inclusion: { in: [-1, 1] }
   validates :author_id,
     uniqueness: { scope: [:votable_type, :votable_id], message: 'You have already voted' },
     not_votable_author: true
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,18 +5,21 @@ RSpec.describe Vote, type: :model do
   it { should belong_to :author }
 
   it { should validate_inclusion_of(:votable_type).in_array(%w(Question Answer)) }
-  it { should validate_presence_of :value }
   it { should validate_inclusion_of(:value).in_array([-1,1]) }
+
   it do
     subject.author = create(:user)
+
     should validate_uniqueness_of(:author_id).scoped_to(:votable_type, :votable_id)
       .with_message('You have already voted')
   end
+
   it 'validates that voting user not author votable object' do
     user = create(:user)
     question = create(:question, author_id: user.id)
     vote = question.votes.build(author_id: user.id, value: 1)
     vote.valid?
+
     expect(vote.errors[:author_id]).to eq(["You can not vote for your question"])
   end
 end
