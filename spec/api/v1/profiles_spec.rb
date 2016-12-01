678511d18f0e65f493ec8a4dd6031308ed55463b
Anikin Artyom <anikinartyom@gmail.com>
2016-12-01 23:01:35 +0500

    refactor get request

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -4,12 +4,12 @@ describe 'Profiles API' do
   describe 'GET /me' do
     context 'unauthorized' do
       it 'returns 401 status if there is no access_token' do
-        get '/api/v1/profiles/me', format: :json
+        get '/api/v1/profiles/me', params: { format: :json }
         expect(response).to have_http_status 401
       end
 
       it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/profiles/me', format: :json, access_token: '123456'
+        get '/api/v1/profiles/me', params: { format: :json, access_token: '123456' }
         expect(response).to have_http_status 401
       end
     end
@@ -18,7 +18,7 @@ describe 'Profiles API' do
       let(:me) { create(:user) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-      before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
+      before { get '/api/v1/profiles/me', params: { format: :json, access_token: access_token.token } }
 
       it 'returns 200 status' do
         expect(response).to be_success
