461fa2b2109946253b894ba8c00bd3348c784f90
Anikin Artyom <anikinartyom@gmail.com>
2016-12-06 15:52:47 +0500

    shared_examples_for acceptance oauth sign in with enter email

--- spec/support/shared/acceptances/oauth_with_enter_email.rb
+++ spec/support/shared/acceptances/oauth_with_enter_email.rb
@@ -1,30 +1,29 @@
 shared_examples_for "Oauth with enter email" do
-  describe 'user sign_in' do
-
+  describe "user sign_in" do
     background { visit new_user_session_path }
 
-    scenario 'user sign_in successfully' do
+    scenario "user sign_in successfully" do
       clear_emails
-      mock_auth_without_email('vkontakte')
-      email = 'user@example.com'
+      mock_auth_without_email(provider.to_s)
+      email = "user@example.com"
 
-      click_on 'Sign in with Vkontakte'
+      click_on "Sign in with #{provider.capitalize}"
 
-      expect(page).to have_content('Email can\'t be blank')
-      fill_in 'Email', with: email
-      click_on 'Sign up'
+      expect(page).to have_content("Email can\'t be blank")
+      fill_in "Email", with: email
+      click_on "Sign up"
 
       open_email(email)
-      current_email.click_link 'Confirm my account'
+      current_email.click_link "Confirm my account"
 
-      expect(page).to have_content 'Your email address has been successfully confirmed'
+      expect(page).to have_content "Your email address has been successfully confirmed"
     end
 
-    context 'with invalid credentials' do
-      scenario 'user not sign_in' do
-        mock_auth_invalid(:vkontakte)
-        click_link 'Sign in with Vkontakte'
-        expect(page).to have_content('Could not authenticate you from Vkontakte because "Credentials are invalid"')
+    context "with invalid credentials" do
+      scenario "user not sign_in" do
+        mock_auth_invalid(provider.to_sym)
+        click_link "Sign in with #{provider.capitalize}"
+        expect(page).to have_content("Could not authenticate you from #{provider.capitalize} because \"Credentials are invalid\"")
       end
     end
   end
