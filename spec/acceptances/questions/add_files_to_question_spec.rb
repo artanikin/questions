f7200ad13e21e000d6e93a6e84c4bfa31d1c73fb
Anikin Artyom <anikinartyom@gmail.com>
2016-12-06 16:14:05 +0500

    shared_examples for acceptance add_files

--- spec/acceptances/questions/add_files_to_question_spec.rb
+++ spec/acceptances/questions/add_files_to_question_spec.rb
@@ -7,34 +7,10 @@ feature 'Add files to questions', %(
 ) do
 
   given(:user) { create(:user) }
+  given(:path) { new_question_path }
+  given(:title?) { true }
+  given(:submit_btn) { "Create" }
+  given(:attachment_block) { ".question" }
 
-  background do
-    sign_in(user)
-    visit new_question_path
-  end
-
-  scenario 'User adds files when asks the question', :js do
-    fill_in 'Title', with: 'Title placeholder'
-    fill_in 'Body', with: 'Placeholder for body'
-    attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
-
-    click_on 'add file'
-
-    within '.nested_fields:nth-child(2)' do
-      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    end
-
-    click_on 'add file'
-
-    within '.nested_fields:nth-child(3)' do
-      attach_file 'File', "#{Rails.root}/spec/acceptance_helper.rb"
-      click_on 'remove file'
-    end
-
-    click_on 'Create'
-
-    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
-    expect(page).to_not have_link 'acceptance_helper.rb', href: '/uploads/attachment/file/3/acceptance_helper.rb'
-  end
+  it_behaves_like "Add attachments"
 end
