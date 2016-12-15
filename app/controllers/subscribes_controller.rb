976b0f7b203f4040a5e319235d25b9bca1a373ff
Anikin Artyom <anikinartyom@gmail.com>
2016-12-15 11:32:51 +0500

    refactoring subscribes controller

--- app/controllers/subscribes_controller.rb
+++ app/controllers/subscribes_controller.rb
@@ -1,34 +1,26 @@
 class SubscribesController < ApplicationController
   before_action :set_question, only: [:create]
+  before_action :set_subscribe, only: [:destroy]
 
   authorize_resource
 
   respond_to :js
 
   def create
-    @subscribe = @current_user.subscribes.build(question_id: @question.id)
-    if @subscribe.save
-      flash.now[:notice] = "You successfully subscribe to question"
-    else
-      flash.now[:alert] = "You can not subscribe to question"
-    end
+    respond_with(@subscribe = @current_user.subscribes.create!(question: @question))
   end
 
   def destroy
-    @subscribe = Subscribe.find(params[:id])
-    @question = @subscribe.question
-
-    if @current_user.author?(@subscribe)
-      @subscribe.destroy
-      flash.now[:notice] = "You successfully unsubscribe to question"
-    else
-      flash.now[:alert] = "You can not unsubscribe to question"
-    end
+    respond_with(@subscribe.destroy)
   end
 
   private
 
   def set_question
     @question = Question.find(params[:question_id])
+  end
+
+  def set_subscribe
+    @subscribe = Subscribe.find(params[:id])
   end
 end
