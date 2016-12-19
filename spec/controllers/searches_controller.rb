75acafc31f670f45de53dedac6160f0ab36d205b
Anikin Artyom <anikinartyom@gmail.com>
2016-12-19 19:54:34 +0500

    add test for searches controller

--- spec/controllers/searches_controller.rb
+++ spec/controllers/searches_controller.rb
@@ -2,9 +2,15 @@ require "rails_helper"
 
 RSpec.describe SearchesController, type: :controller do
   describe "GET #index" do
-    before { get :index, params: { q: "" } }
+    subject { get :index, params: { q: "Search string", object: "all" } }
+
+    it "call search method" do
+      expect(Search).to receive(:search).with("Search string", "all")
+      subject
+    end
 
     it "render index view" do
+      subject
       expect(response).to render_template :index
     end
   end
