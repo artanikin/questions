188c7c676d347fa7bf1d4dd6a10e4b88d962b95e
Anikin Artyom <anikinartyom@gmail.com>
2016-12-22 14:41:35 +0500

    add caching for question page

--- app/models/subscribe.rb
+++ app/models/subscribe.rb
@@ -1,7 +1,7 @@
 class Subscribe < ApplicationRecord
   include Authorable
 
-  belongs_to :question
+  belongs_to :question, touch: true
 
   validates :question_id, uniqueness: { scope: :author_id }
 end
