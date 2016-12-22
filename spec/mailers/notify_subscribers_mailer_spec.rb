a2f00a14003c0ee3960605a22a03da63e4fef33d
Anikin Artyom <anikinartyom@gmail.com>
2016-12-22 14:54:50 +0500

    fix specs

--- spec/mailers/notify_subscribers_mailer_spec.rb
+++ spec/mailers/notify_subscribers_mailer_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe NotifySubscribersMailer, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("Get new answer for question")
       expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(["info@questions.com"])
+      expect(mail.from).to eq(["anikinartyom@gmail.com"])
     end
 
     it "renders the body" do
